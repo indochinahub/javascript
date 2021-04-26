66ee56f930823f134c532e9cf462bc616e4dd5bd
Wittaya  Wijit <indochinahub@gmail.com>
2021-04-26 12:13:42 +0700

    ปรับปรุงเนื้อหา

--- jscripts.js
+++ jscripts.js
@@ -1,6 +1,27 @@
-var a = "Hello, World!";
 
-function b(){
-	console.log("Hello")
-	return 0;
+
+// long running function
+function waitThreeSeconds(){
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms ){}
+    console.log("Finished function")
 }
+
+function clickHandler(){
+    console.log("click Event");
+}
+
+document.addEventListener('click',clickHandler);
+
+waitThreeSeconds();
+console.log("Finished Execution");
+
+waitThreeSeconds();
+console.log("Finished Execution");
+
+
+
+
+
+
+
