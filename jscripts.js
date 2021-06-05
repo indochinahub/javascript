12c31cb7b41be918b16bddc1e4af696279c419b5
Wittaya  Wijit <indochinahub@gmail.com>
2021-06-05 23:23:24 +0700

    commit

--- jscripts.js
+++ jscripts.js
@@ -1,21 +1,48 @@
 
-var person = {
-    firsname : "Wittaya",
-    lastname : "Wijit",
-    getFullName : function(){
-        return this.firsname + "  " + this.lastname;
+function mapForEach(arr, fn){
+
+    var newArr = [];
+    for( var i = 0; i < arr.length ; i++ ){
+        newArr.push(fn(arr[i]))
     }
+    return newArr;
 }
 
-console.log( person.getFullName()); // "Wittaya  Wijit"
+/*******************************/
+var arr1 = [1,2,3];
+console.log(arr1);  // [1,2,3]
 
-(function( language ){
-    console.log(" Logged : " + this.getFullName());
-    console.log(" language : " + language);
+var arr2 = mapForEach(arr1, function(item){
+    return item*2;
+});
+console.log(arr2);  // [2,4,6]
 
-}.apply(person,["en"]))  ; //  "Logged : Wittaya  Wijit"
-                        //  " language : en "
+/*******************************/
+// Pass another function
+var arr3 = mapForEach(arr1, function(item){
+    return item > 1;
+});
+console.log(arr3);  // [false, false, true]
 
+/*******************************/
+var checkPastLimit = function(limiter, item){
+    return item > limiter;
+};
+// we set value of limiter to 1 
+// and checkPastLimit  has only one parameter
+var arr4 = mapForEach( arr1, checkPastLimit.bind(this, 1));
+console.log(arr4);  // [false, true, true]
+
+
+/*******************************/
+var checkPastLimitSimplified = function(limiter){
+    return function(limiter,item){
+        console.log(item, limiter);
+        return item > limiter
+    }.bind(this, limiter);
+};
+var arr5 = mapForEach( arr1, checkPastLimitSimplified(1));
+console.log(arr5);  // [false, true, true]
 
 
 
